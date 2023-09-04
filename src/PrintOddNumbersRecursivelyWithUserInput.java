52d095a027f3fca08d6667985c3896d467caf86d
21002624 <akasharul2407@gmail.com>
2023-09-04 14:35:53 +0530

    first commit

---
+++ src/PrintOddNumbersRecursivelyWithUserInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PrintOddNumbersRecursivelyWithUserInput {
+    public static void main(String[] args) {
+        int current=15;
+        printOddNumbers(current);
+    }
+
+    public static int printOddNumbers(int current) {
+        if(current==1)
+        {
+            return 0;
+        }
+        if (current % 2 != 0) {
+            System.out.println(current);
+            printOddNumbers(current -2);
+        }
+        return 1;
+    }
+}
